c29c5868cde0431e7ba529179b96b2d60ded1cd3
lukkie580 <jonc454588@gmail.com>
2025-10-07 17:45:49 +0100

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,9 +1,9 @@
 //=====💫 sᴛᴀʀ xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263776388689', // add your phone number
+  BOT_NAME: 'aster', // ur bot name
+  OWNER_NAME: 'your father', //your name
+  OWNER_NUMBER: '2348140074277', // add your phone number
   SESSION_ID: '', // use sessin id if inbuilt pairing not working
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare', // ur timezone
